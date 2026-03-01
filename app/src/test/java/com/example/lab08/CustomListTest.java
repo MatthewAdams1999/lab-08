992649f51540983d557896c068cb40b0a4ef07e3
MatthewAdams1999 <157847166+MatthewAdams1999@users.noreply.github.com>
2026-03-01 16:33:51 -0700

    Created test for delete city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,5 +1,7 @@
 package com.example.lab08;
-
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
@@ -11,5 +13,15 @@ public class CustomListTest {
 
         // This will fail initially because hasCity() doesn't exist/is not implemented
         assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
     }
 }
